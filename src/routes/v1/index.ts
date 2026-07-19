020c846c9758d0480f95e7986a6a3b5ed8facca6
Neider Lopez <neider86@protonmail.com>
2026-07-18 19:15:40 -0500

    feat: se importa la ruta de categorias

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import categoryRouter from './category/index';
 import ticketRouter from './ticket/index';
 
 const router: Router = Router();
 
+router.use(categoryRouter);
 router.use(ticketRouter);
 
 export default router;
