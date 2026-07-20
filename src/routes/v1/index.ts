139189d057883ff4b7d6cb78625abb9c388c6acb
Neider Lopez <neider86@protonmail.com>
2026-07-20 12:01:46 -0500

    feat: se importa las rutas de prioridad

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import categoryRouter from './category/index';
 import ticketRouter from './ticket/index';
+import priorityRouter from './priority/index'
 
 const router: Router = Router();
 
 router.use(categoryRouter);
 router.use(ticketRouter);
+router.use(priorityRouter)
 
 export default router;
